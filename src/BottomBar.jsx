661579920ab687c53151e748b2c68a4590f522a3
faesalabid <faesalabid@gmail.com>
2023-12-23 00:59:04 -0500

    Bottom bar update

--- src/BottomBar.jsx
+++ src/BottomBar.jsx
@@ -1,23 +1,35 @@
-
+import React, { useState } from 'react';
 import './bottombar.css';
+import bhomeIcon from './assets/bhome.png'
 import bsearchIcon from './assets/search.svg';
-import baccountIcon from './assets/account.svg';
+import baccountIcon from './assets/baccount.png';
+import bhamburgIcon from './assets/bham.png'
 
 function BottomBar() {
+
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <div className='bottom-container'>
       <div className='bbar'>
         <div className='bicons-container'>
-          <img src={bsearchIcon} alt="" className="bsearch" />
+          <img src={bhomeIcon} alt="" className="bhome" /> 
           <img src={baccountIcon} alt="" className="baccount" />
-          <div className="bhamburg">
-            <ul>
+          <img src={bsearchIcon} alt="" className="bsearch" />
+          
+          <div className="bhamburg" onClick={toggleDropdown}>
+            <img src={bhamburgIcon} alt="" className="bham" />
+            {dropdownVisible && (<ul className='dropup-menu'>
               <li><a href="#">All Products</a></li>
               <li><a href="#">Wholesale</a></li>
               <li><a href="#pages1">About Us</a></li>
               <li><a href="#pages2">Contact</a></li>
               <li><a href="#pages3">Terms and Policies</a></li>
-            </ul>
+            </ul>)}
           </div>
         </div>
       </div>
