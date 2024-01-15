8f057da05eddbaad572c0967e27eb6317b802723
Kamran Habib <kmrncodes@gmail.com>
2024-01-16 01:33:39 +0530

    Removed csc

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import "../styles/checkout.css";
-import csc from 'countries-states-cities';
 function Checkout() {
   // State for form fields
   const [fName, setFName] = useState("");
