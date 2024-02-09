7917e5eeda3a477d6048a87b1873589d3a3e0605
Kamran Habib <kmrncodes@gmail.com>
2024-02-10 03:46:32 +0530

    ui

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -61,7 +61,7 @@ function Contact() {
   }
 
   return (
-    <div className="contact-container">
+    <div className="account-container">
       <PagesHeader />
 
       <form onSubmit={order} className="contact-form">
