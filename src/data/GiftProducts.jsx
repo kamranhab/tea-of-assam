6f4e576d4140dcdadae833a2b078de9989f6d9ec
faesalabid <faesalabid@gmail.com>
2024-02-13 23:46:05 -0500

    data addition

--- src/data/GiftProducts.jsx
+++ src/data/GiftProducts.jsx
@@ -1,12 +1,12 @@
-import giftImg1 from '../assets/giftimage1.webp'
-import giftImg2 from '../assets/giftimage2.webp'
+import gift1Tea from '../assets/products/gift1.png'
+import gift2Tea from '../assets/products/gift2.png'
 
 const GiftProducts=[
     {   
         id: 1001,
-        image:giftImg1,
-        name:'Gift Box',
-        price:'$100.00',
+        image:gift1Tea,
+        name:'Trio Cups Gift Set',
+        price:'$70.99',
         size:'3 items',
         slug:'gift-box',
         desc:'This is soluble instant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water. This is solubleinstant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water. This is soluble instant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water. This is solubleinstant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water.'
@@ -14,9 +14,9 @@ const GiftProducts=[
 
     {    
         id: 1002,
-        image:giftImg2,
-        name:'Tea Bundle',
-        price:'$100.00',
+        image:gift2Tea,
+        name:'Tea-Pot Gift Bundle',
+        price:'$60.49',
         size:'3 items',
         slug:'tea-bundle',
         desc:'This is soluble instant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water. This is solubleinstant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water. This is soluble instant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water. This is solubleinstant coffee. 1 spoon makes 1 cup of 125ml coffee. Drink hot or cold in milk or water.'
